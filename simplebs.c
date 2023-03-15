78acb9b74065415a47358baf8130879f353e9261
mg-ad <127919675+mg-ad@users.noreply.github.com>
2023-03-15 14:27:48 +0530

    Create simplebs.c

---
+++ simplebs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("sum is %d",(a+b));
+    printf("\nremainder is %d",(a%b));
+
+    return 0;
+}
