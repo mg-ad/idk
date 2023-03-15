34adbee601be62fa7e039d6c658a641d314aa1a3
mg-ad <127919675+mg-ad@users.noreply.github.com>
2023-03-15 14:30:20 +0530

    Create idk food.c

---
+++ idk food.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    double b;
+    char c;
+    // Write C code here
+    scanf("%d %c",&a,&c);
+    printf("ate in the mess chapati and rice\n");
+    printf("price:%d\n",a);
+    printf("price divided by 7 is %.4lf\n",b=a/7.0);
+    printf("my favourite character is %c",c);
+    return 0;
